ae32916be99293cb5683a9a1cbc9dbfabaf9e9d8
CrysisDavid <davidsoliss123@gmail.com>
2024-07-11 05:10:45 -0400

    Task 12

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -44,6 +44,7 @@ public_users.get('/isbn/:isbn', async function (req, res) {
     //Write your code here
     //return res.status(300).json({message: "Yet to be implemented"});
     try {
+        const isbn = req.params.isbn;
         const response = await axios.get(`BOOK_SHOP_URL/${isbn}`);
         const book = response.data;
         if (response.result) {
@@ -56,22 +57,38 @@ public_users.get('/isbn/:isbn', async function (req, res) {
 });
 
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
     //Write your code here
     //return res.status(300).json({ message: "Yet to be implemented" });
-    const author = req.params.author;
-    const booksByAuthor = Object.values(books).filter((book) => book.author === author);
-    if (booksByAuthor.length) {
-        return res.send(JSON.stringify({ booksByAuthor }));
+    try {
+        const author = req.params.author;
+        const response = await axios.get(`BOOK_SHOP_URL/author/${author}`);
+        const book = response.data;
+        if (response.result) {
+            return res.send(JSON.stringify(book));
+        }
+        return res.status(500).json({ message: "Not found the books by author" });
+    } catch (error) {
+        return res.status(500).json({ message: "Error fetching books", error: error.message });
     }
-
-    return res.status(500).json({ message: "Not found the books by author" });
 });
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
     //Write your code here
     //return res.status(300).json({ message: "Yet to be implemented" });
+    try {
+        const author = req.params.author;
+        const response = await axios.get(`BOOK_SHOP_URL/author/${author}`);
+        const book = response.data;
+        if (response.result) {
+            return res.send(JSON.stringify(book));
+        }
+        return res.status(500).json({ message: "Not found the books by author" });
+    } catch (error) {
+        return res.status(500).json({ message: "Error fetching books", error: error.message });
+    }
+    
     const title = req.params.title;
     const booksByTitle = Object.values(books).filter((book) => book.title === title);
     if (booksByTitle.length) {
